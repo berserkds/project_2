8bad07ffc0c2b76a9d1d14c0429e43c4d33e46ef
Sergeev Daniil <daniil8778@yandex.ru>
2025-02-04 20:10:27 +0300

    edits_2

--- Delivery.py
+++ Delivery.py
@@ -1,12 +1,12 @@
-# 132812177
-def delivery(data: list, limit: int) -> int:
+# 132828423
+def calculate_platforms(data: list, limit: int) -> int:
     sorted_data: list = sorted(data)
     total: int = 0
     light: int = 0
     heavy: int = len(data) - 1
     while light <= heavy:
         summ = sorted_data[light] + sorted_data[heavy]
-        if (summ == limit or summ < limit):
+        if summ <= limit:
             light += 1
         total += 1
         heavy -= 1
@@ -16,5 +16,5 @@ def delivery(data: list, limit: int) -> int:
 if __name__ == '__main__':
     mas_of_rob = [int(number) for number in input().split()]
     limit_mas = int(input())
-    result = delivery(mas_of_rob, limit_mas)
+    result = calculate_platforms(mas_of_rob, limit_mas)
     print(result)
